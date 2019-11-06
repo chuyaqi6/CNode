1c004007aafd4439610ed0c66cc8fca6a2d459d2
chuyaqi6 <2484917266@qq.com>
2019-11-06 09:43:02 +0800

    add build

---
+++ build/precache-manifest.48ed968ed416b1ff7c7ce5475223348c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4223d3430acd002b4bb8e64070a65fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b958d6bf448a5978544",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "b8ae19e3620364972080",
+    "url": "/static/css/main.04c0559a.chunk.css"
+  },
+  {
+    "revision": "6b958d6bf448a5978544",
+    "url": "/static/js/2.4339a9f1.chunk.js"
+  },
+  {
+    "revision": "b8ae19e3620364972080",
+    "url": "/static/js/main.774e53ec.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
